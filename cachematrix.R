39363aba0e436bd841933eb55866690053f0bb5a
Sriram297 <55543390+Sriram297@users.noreply.github.com>
2020-06-26 11:10:31 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,18 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL
+  inv <- NULL
   set <- function(y){
     x <<- y
     inv <<- NULL
+  }
 
-}
-
- get <- function() {x}
+  get <- function() {x}
   setInverse <- function(inverse) {inv <<- inverse}
   getInverse <- function() {inv}
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-}
+  }
+
 
 ## Write a short comment describing this function
 
@@ -31,4 +31,3 @@ inv <- solve(mat, ....)
 x$setInverse(inv)
 inv
 }
-
